1ad109e71c4da9649a33ea35cf0fdf2f61ef2b4d
Mykhail Poda <mykhail_poda@epam.com>
2021-03-21 18:23:26 +0200

    add default policies

--- locals.tf
+++ locals.tf
@@ -6,4 +6,8 @@ locals {
   pilon_security_group_name = format("%s-%s-%s-%s", var.tags.Environment, var.tags.Product, var.tags.Sub_product, "sg")
   pilon_launch_template_name = format("%s-%s-%s-%s", var.tags.Environment, var.tags.Product, var.tags.Sub_product, "lt")
   pilon_instance_profile_name = format("%s-%s-%s-%s", var.tags.Environment, var.tags.Product, var.tags.Sub_product, "profile")
+
+  default_policies = [
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  ]
 }
