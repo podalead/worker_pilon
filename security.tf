c3a5dd93f905715f046af7d6ef61ccfa09228de6
Mykhail Poda <mykhail_poda@epam.com>
2021-03-21 17:54:48 +0200

    update ssh protocol

--- security.tf
+++ security.tf
@@ -5,16 +5,14 @@ resource "aws_security_group" "pilon_common_security_group" {
     from_port = 0
     protocol = "-1"
     to_port = 0
-    cidr_blocks = [
-      "0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port = 22
-    protocol = "-1"
+    protocol = "tcp"
     to_port = 22
-    cidr_blocks = [
-      "0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = merge(
